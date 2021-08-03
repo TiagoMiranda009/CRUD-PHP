e8527bbf771e32b6b5e8032209e0eda1b2f9b90a
Tiago Miranda <tiagomiranda009@gmail.com>
2021-08-03 19:00:12 -0300

    Quase lagit add *

---
+++ README.md
@@ -0,0 +1,26 @@
+# Projeto CRUD em PHP - Bootstrap, PDO & MySQL
+
+Desenvolvimento de um simples projeto CRUD para uma entrevista de trabalho, utilizando o acesso a banco de dados com o MySQL e linguagem PHP.
+
+## Assuntos Abordados no Desenvolvimento do Projeto:
+
+- Acesso a banco de dados com o MySql.
+- Otimização da conexão com o banco de dados através do PDO (PHP Data Object).
+- Uso de linguagens, como: JavaScript e CSS.
+- Uso do framework Bootstrap e Jquery para realização de um layout responsivo para o projeto.
+
+## Configuração do Projeto:
+
+- Executar a query db_crud.sql ou importar o arquivo no phpMyAdmin para criar a table necessária.
+- Editar o arquivo **banco.php** 
+
+```
+$dbNome = 'nomeDaTable' 
+$dbHost = 'nomeDoDominioOuIP:Porta' 
+$dbUsuario = 'usuarioDoMysql' 
+$dbSenha 'senhaDoUsuario'
+
+```
+
+
+PS: Estou ciente do problema de inserir a imagem do BD.
---
+++ update.php
@@ -0,0 +1,170 @@
+<?php
+
+require 'banco.php';
+
+$id = null;
+if (!empty($_GET['id'])) {
+    $id = $_REQUEST['id'];
+}
+
+if (null == $id) {
+    header("Location: index.php");
+}
+
+if (!empty($_POST)) {
+
+    $nomeErro = null;
+    $enderecoErro = null;
+    $telefoneErro = null;
+    $emailErro = null;
+    $fotoErro = null;
+
+    $nome = $_POST['nome'];
+    $telefone = $_POST['telefone'];
+    $email = $_POST['email'];
+    $foto = $_POST['foto'];
+
+    //Validação
+    $validacao = true;
+    if (empty($nome)) {
+        $nomeErro = 'Por favor digite o nome!';
+        $validacao = false;
+    }
+
+    if (empty($email)) {
+        $emailErro = 'Por favor digite o email!';
+        $validacao = false;
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $emailErro = 'Por favor digite um email válido!';
+        $validacao = false;
+    }
+
+    if (empty($endereco)) {
+        $enderecoErro = 'Por favor digite o endereço!';
+        $validacao = false;
+    }
+
+    if (empty($telefone)) {
+        $telefoneErro = 'Por favor digite o telefone!';
+        $validacao = false;
+    }
+
+    if (empty($sexo)) {
+        $fotoErro = 'Por favor erro a inserir a foto!';
+        $validacao = true;
+    }
+
+    // update data
+    if ($validacao) {
+        $pdo = Banco::conectar();
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE usuarios  set nome = ?, telefone = ?, email = ?, foto = ? WHERE id = ?";
+        $q = $pdo->prepare($sql);
+        $q->execute(array($nome, $telefone, $email, $foto, $id));
+        Banco::desconectar();
+        header("Location: index.php");
+    }
+} else {
+    $pdo = Banco::conectar();
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM usuarios where id = ?";
+    $q = $pdo->prepare($sql);
+    $q->execute(array($id));
+    $data = $q->fetch(PDO::FETCH_ASSOC);
+    $nome = $data['nome'];
+    $telefone = $data['telefone'];
+    $email = $data['email'];
+    $sexo = $data['foto'];
+    Banco::desconectar();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!-- using new bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Atualizar Contato</title>
+</head>
+
+<body>
+<div class="container">
+
+    <div class="span10 offset1">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="well"> Atualizar Contato </h3>
+            </div>
+            <div class="card-body">
+                <form class="form-horizontal" action="update.php?id=<?php echo $id ?>" method="post">
+
+                    <div class="control-group <?php echo !empty($nomeErro) ? 'error' : ''; ?>">
+                        <label class="control-label">Nome</label>
+                        <div class="controls">
+                            <input name="nome" class="form-control" size="50" type="text" placeholder="Nome"
+                                   value="<?php echo !empty($nome) ? $nome : ''; ?>">
+                            <?php if (!empty($nomeErro)): ?>
+                                <span class="text-danger"><?php echo $nomeErro; ?></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <div class="control-group <?php echo !empty($telefoneErro) ? 'error' : ''; ?>">
+                        <label class="control-label">Telefone</label>
+                        <div class="controls">
+                            <input name="telefone" class="form-control" size="30" type="text" placeholder="Telefone"
+                                   value="<?php echo !empty($telefone) ? $telefone : ''; ?>">
+                            <?php if (!empty($telefoneErro)): ?>
+                                <span class="text-danger"><?php echo $telefoneErro; ?></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <div class="control-group <?php echo !empty($emailErro) ? 'error' : ''; ?>">
+                        <label class="control-label">Email</label>
+                        <div class="controls">
+                            <input name="email" class="form-control" size="40" type="text" placeholder="Email"
+                                   value="<?php echo !empty($email) ? $email : ''; ?>">
+                            <?php if (!empty($emailErro)): ?>
+                                <span class="text-danger"><?php echo $emailErro; ?></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <div class="control-group <?php !empty($fotoErro) ? '$fotoErro ' : ''; ?>">
+                        <label class="control-label">Foto</label>
+                        <div class="controls">
+                            <input size="80" class="form-control" name="foto" type="file" placeholder="Foto"
+                                   value="<?php echo !empty($foto) ? $foto : ''; ?>">
+                            <?php if (!empty($fotoErro)): ?>
+                                <span class="text-danger"><?php echo $fotoErro; ?></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+
+                    <br/>
+                    <div class="form-actions">
+                        <button type="submit" class="btn btn-warning">Atualizar</button>
+                        <a href="index.php" type="btn" class="btn btn-default">Voltar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
